44a27457062fcf41adcefca04bbac2d2a40519ff
absar22 <absarahmad137@gmail.com>
2026-09-07 15:40:33 +0530

    setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloudinary_url: process.env.CLOUDINARY_URL
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded succcesfully
+        console.log("File uploaded successfully on cloudinary:", response.url)
+        return response
+
+    }catch(error){
+        // remove locally saved temperory file as the upload operation failed
+       fs.unlinkSync(localFilePath)
+       return null
+    }
+}
+
+
+export default uploadOnCloudinary
