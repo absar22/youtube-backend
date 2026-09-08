c1b6df0187797e59967a697d36c9574afeab25f3
absar22 <absarahmad137@gmail.com>
2026-09-08 22:02:44 +0530

    completed the userRegisteration controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/apiError.js'
 import {User} from '../models/user.model.js'
+import { upload } from '../middlewares/multer.middleware.js'
+import { ApiResponse } from '../utils/apiResponse.js'
 const registerUser  = asyncHandler(async (req,res) => {
     // get user details from frontend
     // validate user details
@@ -26,10 +28,35 @@ const registerUser  = asyncHandler(async (req,res) => {
         throw new ApiError(409, 'User already exists')
     }
 
-    const avatar = req.files.avatar[0]
-    const coverImage = req.files.coverImage[0]
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
 
-    
+    if(!avatarLocalPath){
+        throw new ApiError(401, 'Avatar is required')
+    }
+
+    const avatar = await upload(avatarLocalPath)
+    const coverImage = await upload(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(500, 'Error uploading Avatar')
+    }
+    const user = await User.create({
+        username,
+        email,
+        fullname,
+        password,
+        avatar,
+        coverImage: coverImage?.url || ""
+    })
+
+     const createdUser = await User.findById(user._id).select('-password  refreshToken')
+     if(!createdUser){
+         throw new ApiError(500, 'Error fetching created user')
+     }
+     
+     return res.status(201).json(
+        new ApiResponse(201, createdUser, "User registered Successfully")
+    )
 
 })
 
