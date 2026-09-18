24fed2deee425fc812cd33184e0a9577eb88cc9b
absar22 <absarahmad137@gmail.com>
2026-09-18 10:36:22 +0530

    added get current user controler logic and its route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword} from "../controllers/user.controller.js"
+import { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import {verifyJWT} from '../middlewares/auth.middleware.js'
 
@@ -19,5 +19,6 @@ router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refreshToken').post(refreshAccessToken)
 router.route('/change-password').patch(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
 
 export default router
